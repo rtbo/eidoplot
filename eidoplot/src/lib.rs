f90641c99da4bcbabca11b7693fa11d5d442b436
Rémi Thebault <remi.thebault@gmail.com>
2025-08-06 10:17:25 +0200

    move mod missing_params to lib.rs

--- eidoplot/src/lib.rs
+++ eidoplot/src/lib.rs
@@ -9,4 +9,26 @@ pub mod render;
 pub mod style;
 pub mod text;
 
-mod missing_params;
+/// Module containing missing configuration values
+/// Basically we put here all magic values that would require proper parameters
+mod missing_params {
+    use crate::{
+        geom,
+        style::{Color, color},
+        text::DEFAULT_FONT_FAMILY,
+    };
+
+    pub const FIG_TITLE_MARGIN: f32 = 6.0;
+    pub const FIG_TITLE_COLOR: Color = color::BLACK;
+    pub const TICK_SIZE: f32 = 4.0;
+    pub const TICK_COLOR: Color = color::BLACK;
+    pub const TICK_LABEL_FONT_FAMILY: &str = DEFAULT_FONT_FAMILY;
+    pub const TICK_LABEL_FONT_SIZE: f32 = 10.0;
+    pub const TICK_LABEL_MARGIN: f32 = 4.0;
+    pub const AXIS_PADDING: geom::Padding = geom::Padding::Custom {
+        t: 0.0,
+        r: 0.0,
+        b: 30.0,
+        l: 50.0,
+    };
+}
