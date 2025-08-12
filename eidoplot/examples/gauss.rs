a42b0b69fa305f7992b81cec3123c1a21174a84a
Rémi Thebault <remi.thebault@gmail.com>
2025-08-12 08:20:11 +0200

    gauss example tweaking

--- eidoplot/examples/gauss.rs
+++ eidoplot/examples/gauss.rs
@@ -3,7 +3,7 @@ use eidoplot::{ir, style};
 use eidoplot_pxl::PxlSurface;
 use eidoplot_svg::SvgSurface;
 
-use rand::rngs::ThreadRng;
+use rand::SeedableRng;
 use rand_distr::{Distribution, Normal};
 
 use std::sync::Arc;
@@ -12,11 +12,14 @@ use std::{env, f64::consts::PI};
 fn main() {
     const MU: f64 = 13.0;
     const SIGMA: f64 = 2.0;
-    const N: usize = 100;
+    const N_DIST: usize = 100;
+    const N_POP: usize = 1000;
+
     const X_MIN: f64 = MU - 4.0 * SIGMA;
     const X_MAX: f64 = MU + 4.0 * SIGMA;
-    let x = (0..N)
-        .map(|i| X_MIN + (i as f64 * (X_MAX - X_MIN) / (N - 1) as f64))
+
+    let x = (0..N_DIST)
+        .map(|i| X_MIN + (i as f64 * (X_MAX - X_MIN) / (N_DIST - 1) as f64))
         .collect::<Vec<f64>>();
     let y = x
         .iter()
@@ -25,12 +28,12 @@ fn main() {
 
     let points = x.iter().zip(y.iter()).map(|(&x, &y)| (x, y)).collect();
 
-    let mut rng = ThreadRng::from_seed(0);
+    let mut rng = predictable_rng(None);
     let normal = Normal::new(MU, SIGMA).unwrap();
-    let pop = (0..N).map(|_| normal.sample(&mut rng)).collect();
+    let pop = (0..N_POP).map(|_| normal.sample(&mut rng)).collect();
 
     let title = ir::Text::new(
-        "Normal distribution",
+        format!("Normal distribution (\u{03bc}={}, \u{03c3}={})", MU, SIGMA),
         style::Font::new("serif".into(), 24.0)
             .with_style(style::font::Style::Italic)
             .with_weight(style::font::Weight::BOLD),
@@ -40,14 +43,14 @@ fn main() {
         X_MIN, X_MAX,
     )))
     .with_label("x".into());
-    let y_axis = ir::Axis::new(ir::axis::Scale::default()).with_label("y".into());
+    let y_axis = ir::Axis::new(ir::axis::Scale::default()).with_label("population density".into());
 
     let dist_series = ir::plot::Series {
         name: Some("distribution".into()),
         plot: ir::plot::SeriesPlot::Xy(ir::plot::XySeries {
             line: style::Line {
-                color: style::color::BLUE,
-                width: 3.0,
+                color: style::color::DARKSLATEBLUE,
+                width: 4.0,
                 pattern: style::LinePattern::Solid,
             },
             points,
@@ -56,16 +59,12 @@ fn main() {
     let pop_series = ir::plot::Series {
         name: Some("population".into()),
         plot: ir::plot::SeriesPlot::Histogram(ir::plot::HistogramSeries {
-            fill: style::color::PURPLE.into(),
-            line: Some(style::Line {
-                color: style::color::RED,
-                width: 3.0,
-                pattern: style::LinePattern::Solid,
-            }),
-            bins: 10,
+            fill: style::color::CORNFLOWERBLUE.with_opacity(0.9).into(),
+            line: None,
+            bins: 16,
             density: true,
             points: pop,
-        })
+        }),
     };
 
     let plot = ir::Plot {
@@ -82,6 +81,11 @@ fn main() {
     let fig = ir::Figure::new(ir::figure::Plots::Plot(plot)).with_title(Some(title));
 
     save_figure(&fig);
+}
+
+fn predictable_rng(seed: Option<u64>) -> impl rand::Rng {
+    let seed = seed.unwrap_or(586350478348);
+    rand_chacha::ChaCha8Rng::seed_from_u64(seed)
 }
 
 fn save_figure(fig: &ir::Figure) {
