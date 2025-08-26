9c8a1f7749ac59c788b65d061f162fde37e9351d
Rémi Thebault <remi.thebault@gmail.com>
2025-08-26 23:15:50 +0200

    new Iris example without polars

--- eidoplot/examples/iris_polars.rs
+++ eidoplot/examples/iris_polars.rs
@@ -1,9 +1,10 @@
 use std::path;
 
-use eidoplot::data;
-use eidoplot::data::Source;
 use eidoplot::ir;
+use eidoplot::data;
 use eidoplot::style;
+
+use polars::prelude::*;
 
 mod common;
 
@@ -13,90 +14,86 @@ fn iris_csv_path() -> path::PathBuf {
     parent.join("Iris.csv")
 }
 
-/// Returns a boolean mask where the column matches the given category
-/// Returns None if the column is not string-like
-fn category_mask<C>(column: &C, category: &str) -> Option<Vec<bool>>
-where
-    C: data::Column + ?Sized,
-{
-    let mask = column.str()?.iter().map(|v| v == Some(category)).collect();
-    Some(mask)
-}
-
-/// Filters a numeric column by a boolean mask
-/// Returns None if the column is not numeric and panics if the lengths do not match
-fn filter_numeric_by_mask<C>(num_col: &C, mask: &[bool]) -> Option<data::VecColumn>
-where
-    C: data::Column + ?Sized,
-{
-    assert_eq!(num_col.len(), mask.len());
-
-    let vec: Vec<f64> = num_col
-        .f64()?
-        .iter()
-        .zip(mask.iter())
-        .filter_map(|(v, &m)| if m { Some(v) } else { None })
-        .map(|v| v.unwrap_or(f64::NAN))
-        .collect();
-    Some(vec.into())
-}
-
 fn main() {
     let iris_csv = iris_csv_path();
-    let csv_data = std::fs::read_to_string(&iris_csv).unwrap();
 
-    let table = eidoplot_utils::parse_csv_data(&csv_data, ',').unwrap();
+    let df = CsvReadOptions::default()
+        .try_into_reader_with_file_path(Some(iris_csv))
+        .unwrap()
+        .finish()
+        .unwrap();
 
-    let species = table.column("Species").unwrap();
-    let sepal_length = table.column("SepalLengthCm").unwrap();
-    let petal_length = table.column("PetalLengthCm").unwrap();
-
-    let setosa_mask = category_mask(species, "Iris-setosa").unwrap();
-    let versicolor_mask = category_mask(species, "Iris-versicolor").unwrap();
-    let virginica_mask = category_mask(species, "Iris-virginica").unwrap();
-
-    let setosa_sepal_length = filter_numeric_by_mask(sepal_length, &setosa_mask).unwrap();
-    let setosa_petal_length = filter_numeric_by_mask(petal_length, &setosa_mask).unwrap();
-
-    let versicolor_sepal_length = filter_numeric_by_mask(sepal_length, &versicolor_mask).unwrap();
-    let versicolor_petal_length = filter_numeric_by_mask(petal_length, &versicolor_mask).unwrap();
-
-    let virginica_sepal_length = filter_numeric_by_mask(sepal_length, &virginica_mask).unwrap();
-    let virginica_petal_length = filter_numeric_by_mask(petal_length, &virginica_mask).unwrap();
+    let setosa = df
+        .clone()
+        .lazy()
+        .filter(col("Species").eq(lit("Iris-setosa")))
+        .collect()
+        .unwrap();
+    let versicolor = df
+        .clone()
+        .lazy()
+        .filter(col("Species").eq(lit("Iris-versicolor")))
+        .collect()
+        .unwrap();
+    let virginica = df
+        .lazy()
+        .filter(col("Species").eq(lit("Iris-virginica")))
+        .collect()
+        .unwrap();
 
     let mut source = data::NamedColumns::new();
 
     source.add_column(
         "setosa_sepal_length",
-        &setosa_sepal_length as &dyn data::Column,
+        setosa
+            .column("SepalLengthCm")
+            .unwrap()
+            .as_materialized_series(),
     );
     source.add_column(
         "setosa_petal_length",
-        &setosa_petal_length as &dyn data::Column,
+        setosa
+            .column("PetalLengthCm")
+            .unwrap()
+            .as_materialized_series(),
     );
 
     source.add_column(
         "versicolor_sepal_length",
-        &versicolor_sepal_length as &dyn data::Column,
+        versicolor
+            .column("SepalLengthCm")
+            .unwrap()
+            .as_materialized_series(),
     );
     source.add_column(
         "versicolor_petal_length",
-        &versicolor_petal_length as &dyn data::Column,
+        versicolor
+            .column("PetalLengthCm")
+            .unwrap()
+            .as_materialized_series(),
     );
 
     source.add_column(
         "virginica_sepal_length",
-        &virginica_sepal_length as &dyn data::Column,
+        virginica
+            .column("SepalLengthCm")
+            .unwrap()
+            .as_materialized_series(),
     );
     source.add_column(
         "virginica_petal_length",
-        &virginica_petal_length as &dyn data::Column,
+        virginica
+            .column("PetalLengthCm")
+            .unwrap()
+            .as_materialized_series(),
     );
 
     let title = "Iris dataset".into();
 
-    let x_axis = ir::Axis::new(ir::axis::Scale::default()).with_label("Sepal Length [cm]".into());
-    let y_axis = ir::Axis::new(ir::axis::Scale::default()).with_label("Petal Length [cm]".into());
+    let x_axis = ir::Axis::new(ir::axis::Scale::default())
+        .with_label("Sepal Length [cm]".into());
+    let y_axis = ir::Axis::new(ir::axis::Scale::default())
+        .with_label("Petal Length [cm]".into());
 
     let setosa = ir::Series {
         name: Some("Setosa".into()),
