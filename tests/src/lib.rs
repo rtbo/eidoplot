945c854b3974399826331750cfec7b0a47812303
Rémi Thebault <remi.thebault@gmail.com>
2025-09-04 23:57:34 +0200

    TestHarness

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -38,52 +38,6 @@ fn bw_theme() -> impl style::Theme {
     style::theme::Light::new(style::series::BLACK)
 }
 
-fn fig_to_pxl<T>(fig: &ir::Figure, theme: T) -> tiny_skia::Pixmap
-where
-    T: style::Theme,
-{
-    let size = fig.size();
-    let fontdb = Arc::new(eidoplot::bundled_font_db());
-    let mut pxl = PxlSurface::new(
-        size.width() as u32,
-        size.height() as u32,
-        Some(fontdb.clone()),
-    )
-    .unwrap();
-    pxl.draw_figure(
-        &fig,
-        &(),
-        theme,
-        drawing::Options {
-            fontdb: Some(fontdb),
-        },
-    )
-    .unwrap();
-    pxl.into_pixmap()
-}
-
-fn fig_to_svg<T>(fig: &ir::Figure, theme: T) -> Vec<u8>
-where
-    T: style::Theme,
-{
-    let size = fig.size();
-    let mut svg = SvgSurface::new(size.width() as u32, size.height() as u32);
-    svg.draw_figure(&fig, &(), theme, drawing::Options::default())
-        .unwrap();
-    let mut buf = Vec::new();
-    svg.write(&mut buf).unwrap();
-    buf
-}
-
-fn pxl_to_file(pxl: &tiny_skia::Pixmap, file: &Path) {
-    std::fs::create_dir_all(file.parent().unwrap()).unwrap();
-    pxl.save_png(file).unwrap();
-}
-
-fn svg_to_file(svg: &[u8], file: &Path) {
-    std::fs::create_dir_all(file.parent().unwrap()).unwrap();
-    std::fs::write(file, svg).unwrap();
-}
 
 #[cfg(feature = "regenerate-refs")]
 const REGENERATE_REFS: bool = true;
@@ -91,24 +45,152 @@ const REGENERATE_REFS: bool = true;
 #[cfg(not(feature = "regenerate-refs"))]
 const REGENERATE_REFS: bool = false;
 
-#[cfg(feature = "allow-no-ref")]
-const ALLOW_NO_REF: bool = true;
+struct PreparedAssertion<F> {
+    actual_drawn_fig: F,
+    ref_drawn_fig: F,
+}
 
-#[cfg(not(feature = "allow-no-ref"))]
-const ALLOW_NO_REF: bool = false;
+trait TestHarness {
+    type DrawnFig;
+
+    fn draw<T>(fig: &ir::Figure, theme: T) -> Self::DrawnFig
+    where
+        T: style::Theme;
+
+    fn regenerate_refs() -> bool;
+
+    fn serialize_fig(file: &Path, fig: &Self::DrawnFig);
+
+    fn deserialize_fig(file: &Path) -> Self::DrawnFig;
+
+    fn prepare_assertion<T>(
+        ref_file_name: &str,
+        fig: &ir::Figure,
+        theme: T,
+    ) -> PreparedAssertion<Self::DrawnFig>
+    where
+        T: style::Theme,
+    {
+        let ref_file = ref_file_path(&ref_file_name);
+        let actual_file = actual_file_path(&ref_file_name);
+
+        let actual_drawn_fig = Self::draw(fig, theme);
+
+        if Self::regenerate_refs() {
+            std::fs::create_dir_all(ref_file.parent().unwrap()).unwrap();
+            Self::serialize_fig(&ref_file, &actual_drawn_fig);
+        }
+
+        if !std::fs::exists(&ref_file).unwrap() {
+            std::fs::create_dir_all(actual_file.parent().unwrap()).unwrap();
+            Self::serialize_fig(actual_file.as_path(), &actual_drawn_fig);
+            panic!(
+                "No such ref: \"{}\"\n  Actual figure written to {}",
+                &ref_file_name,
+                actual_file.display()
+            );
+        }
+
+        let ref_drawn_fig = Self::deserialize_fig(&ref_file);
+
+        PreparedAssertion {
+            actual_drawn_fig,
+            ref_drawn_fig,
+        }
+    }
+}
+
+struct PxlHarness;
+
+impl TestHarness for PxlHarness {
+    type DrawnFig = tiny_skia::Pixmap;
+
+    fn draw<T>(fig: &ir::Figure, theme: T) -> Self::DrawnFig
+    where
+        T: style::Theme,
+    {
+        let size = fig.size();
+        let fontdb = Arc::new(eidoplot::bundled_font_db());
+        let mut pxl = PxlSurface::new(
+            size.width() as u32,
+            size.height() as u32,
+            Some(fontdb.clone()),
+        )
+        .unwrap();
+        pxl.draw_figure(
+            &fig,
+            &(),
+            theme,
+            drawing::Options {
+                fontdb: Some(fontdb),
+            },
+        )
+        .unwrap();
+        pxl.into_pixmap()
+    }
+
+    fn regenerate_refs() -> bool {
+        REGENERATE_REFS
+            || std::env::var("EIDOPLOT_TEST_REGENERATE_REFS").is_ok()
+            || std::env::var("EIDOPLOT_TEST_REGENERATE_PNG_REFS").is_ok()
+    }
+
+    fn serialize_fig(file: &Path, fig: &Self::DrawnFig) {
+        fig.save_png(file).unwrap();
+    }
+
+    fn deserialize_fig(file: &Path) -> Self::DrawnFig {
+        tiny_skia::Pixmap::load_png(file).unwrap()
+    }
+}
+
+struct SvgHarness;
+
+impl TestHarness for SvgHarness {
+    type DrawnFig = Vec<u8>;
+
+    fn draw<T>(fig: &ir::Figure, theme: T) -> Self::DrawnFig
+    where
+        T: style::Theme,
+    {
+        let size = fig.size();
+        let mut svg = SvgSurface::new(size.width() as u32, size.height() as u32);
+        svg.draw_figure(&fig, &(), theme, drawing::Options::default())
+            .unwrap();
+        let mut buf = Vec::new();
+        svg.write(&mut buf).unwrap();
+        buf
+    }
+
+    fn regenerate_refs() -> bool {
+        REGENERATE_REFS
+            || std::env::var("EIDOPLOT_TEST_REGENERATE_REFS").is_ok()
+            || std::env::var("EIDOPLOT_TEST_REGENERATE_SVG_REFS").is_ok()
+    }
+
+    fn serialize_fig(file: &Path, fig: &Self::DrawnFig) {
+        std::fs::write(file, fig).unwrap();
+    }
+
+    fn deserialize_fig(file: &Path) -> Self::DrawnFig {
+        std::fs::read(file).unwrap()
+    }
+}
 
 macro_rules! assert_fig_eq_ref {
-    (__assert_pxl, $actual_pxl:expr, $ref_pxl:expr, $file:expr) => {
-        let actual_file = $crate::actual_file_path($file);
-        let diff_file = $crate::diff_file_path($file);
+    (__assert_pxl, $ref_file_name:expr, $prepared:expr) => {
+        let actual_file = $crate::actual_file_path(&$ref_file_name);
+        let diff_file = $crate::diff_file_path(&$ref_file_name);
 
-        let (diff_pxl, diff_count) = $crate::pixelmatch::pixelmatch($actual_pxl.as_ref(), $ref_pxl.as_ref(), Default::default());
+        let (diff_pxl, diff_count) = $crate::pixelmatch::pixelmatch(
+            $prepared.actual_drawn_fig.as_ref(),
+            $prepared.ref_drawn_fig.as_ref(), Default::default());
 
         if diff_count != 0 {
-            let ref_file = $crate::ref_file_path($file);
+            let ref_file = $crate::ref_file_path(&$ref_file_name);
 
-            $crate::pxl_to_file(&$actual_pxl, &actual_file);
-            $crate::pxl_to_file(diff_pxl.as_ref().unwrap(), &diff_file);
+            PxlHarness::serialize_fig(actual_file.as_path(), &$prepared.actual_drawn_fig);
+            PxlHarness::serialize_fig(diff_file.as_path(), diff_pxl.as_ref().unwrap());
 
             panic!(
                 "PXL assertion failed\n actual figure: {:?}\n    ref figure: {:?}\n    diff image: {:?}\n    diff count: {}",
@@ -123,11 +205,12 @@ macro_rules! assert_fig_eq_ref {
             }
         }
     };
-    (__assert_svg, $actual_svg:expr, $ref_svg:expr, $file:expr) => {
-        let actual_file = $crate::actual_file_path($file);
-        if $actual_svg != $ref_svg {
-            let ref_file = $crate::ref_file_path($file);
-            $crate::svg_to_file(&$actual_svg, &actual_file);
+    (__assert_svg, $ref_file_name:expr, $prepared:expr) => {
+        let actual_file = $crate::actual_file_path(&$ref_file_name);
+
+        if $prepared.actual_drawn_fig != $prepared.ref_drawn_fig {
+            let ref_file = $crate::ref_file_path(&$ref_file_name);
+            SvgHarness::serialize_fig(&actual_file, &$prepared.actual_drawn_fig);
             panic!(
                 "SVG assertion failed\n actual figure: {:?}\n    ref figure: {:?}",
                 actual_file, ref_file
@@ -139,53 +222,17 @@ macro_rules! assert_fig_eq_ref {
         }
     };
 
-    (pxl, $fig:expr, $file:expr, $theme:expr) => {
-        let actual_pxl = $crate::fig_to_pxl($fig, $theme);
-        let ref_file = $crate::ref_file_path($file);
-
-        if $crate::REGENERATE_REFS
-            || std::env::var("EIDOPLOT_TEST_REGENERATE_REFS").is_ok()
-            || std::env::var("EIDOPLOT_TEST_REGENERATE_PNG_REFS").is_ok()
-        {
-            $crate::pxl_to_file(&actual_pxl, &ref_file);
-        }
-
-        if !std::fs::exists(&ref_file).unwrap() {
-            let actual_file = $crate::actual_file_path($file);
-            $crate::pxl_to_file(&actual_pxl, &actual_file);
-            if !$crate::ALLOW_NO_REF {
-                panic!("No such ref: \"{}\"\n  Actual figure written to {}", $file, actual_file.display());
-            }
-        } else {
-            let ref_pxl = tiny_skia::Pixmap::load_png(&ref_file).unwrap();
-            assert_fig_eq_ref!(__assert_pxl, actual_pxl, ref_pxl, $file);
-        }
+    (pxl, $fig:expr, $ref_file_name:expr, $theme:expr) => {
+        let prepared = $crate::PxlHarness::prepare_assertion($ref_file_name, $fig, $theme);
+        assert_fig_eq_ref!(__assert_pxl, $ref_file_name, prepared);
     };
     (pxl, $fig:expr, $file:expr) => {
         assert_fig_eq_ref!(pxl, fig, expected, $crate::bw_theme());
     };
 
-    (svg, $fig:expr, $file:expr, $theme:expr) => {
-        let actual_svg = $crate::fig_to_svg($fig, $theme);
-        let ref_file = $crate::ref_file_path($file);
-
-        if $crate::REGENERATE_REFS
-            || std::env::var("EIDOPLOT_TEST_REGENERATE_REFS").is_ok()
-            || std::env::var("EIDOPLOT_TEST_REGENERATE_SVG_REFS").is_ok()
-        {
-            svg_to_file(&actual_svg, &ref_file);
-        }
-
-        if !std::fs::exists(&ref_file).unwrap() {
-            let actual_file = $crate::actual_file_path($file);
-            svg_to_file(&actual_svg, &actual_file);
-            if !$crate::ALLOW_NO_REF {
-                panic!("No such ref: \"{}\"\n  Actual figure written to {}", $file, actual_file.display());
-            }
-        } else {
-            let ref_svg = std::fs::read(&ref_file).unwrap();
-            assert_fig_eq_ref!(__assert_svg, actual_svg, ref_svg, $file);
-        }
+    (svg, $fig:expr, $ref_file_name:expr, $theme:expr) => {
+        let prepared = $crate::SvgHarness::prepare_assertion($ref_file_name, $fig, $theme);
+        assert_fig_eq_ref!(__assert_svg, $ref_file_name, prepared);
     };
     (svg, $fig:expr, $file:expr) => {
         assert_fig_eq_ref!(svg, fig, expected, $crate::bw_theme());
