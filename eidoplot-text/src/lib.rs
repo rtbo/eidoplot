3e35937754150268a9667a9ce9cb30fe777b7ce2
Rémi Thebault <remi.thebault@gmail.com>
2025-09-06 13:28:18 +0200

    eidoplot_text exports BBox

--- eidoplot-text/src/lib.rs
+++ eidoplot-text/src/lib.rs
@@ -8,8 +8,8 @@ pub mod layout;
 pub mod render;
 pub mod shape;
 
-pub use font::{Font, parse_font_families, ScaledMetrics};
-pub use layout::{Anchor, HorAlign, LineVerAlign, TextLayout, VerAlign};
+pub use font::{Font, ScaledMetrics, parse_font_families};
+pub use layout::{Anchor, BBox, HorAlign, LineVerAlign, TextLayout, VerAlign};
 pub use render::{render_text, render_text_tiny_skia};
 pub use shape::{Direction, TextShape};
 
