612f69aae74712e7a30ba53bbaf4976a6357bb67
Rémi Thebault <remi.thebault@gmail.com>
2025-09-19 23:52:41 +0200

    plot padding is set to 0

--- eidoplot/src/lib.rs
+++ eidoplot/src/lib.rs
@@ -48,7 +48,7 @@ mod missing_params {
 
     pub const FIG_TITLE_MARGIN: f32 = 6.0;
 
-    pub const PLOT_PADDING: geom::Padding = geom::Padding::Even(5.0);
+    pub const PLOT_PADDING: geom::Padding = geom::Padding::Even(0.0);
     pub const AXIS_TITLE_MARGIN: f32 = 8.0;
 
     pub const TICK_SIZE: f32 = 4.0;
