a0c6a702d54814da499c9885080e2a171901cf53
Rémi Thebault <remi.thebault@gmail.com>
2025-09-20 08:31:34 +0200

    Update bode example

--- eidoplot/examples/rlc_bode.rs
+++ eidoplot/examples/rlc_bode.rs
@@ -1,6 +1,6 @@
 use std::f64::consts::PI;
 
-use eidoplot::{data, eplt};
+use eidoplot::{data, eplt, ir};
 
 mod common;
 
@@ -12,12 +12,11 @@ fn rlc_load_response(frequencies: &[f64], r: f64, l: f64, c: f64) -> (Vec<f64>, 
     let mut phases = Vec::with_capacity(frequencies.len());
 
     for &f in frequencies {
-        let omega = 2.0 * PI * f;
-        let omega_sq = omega.powi(2);
+        let pulse = 2.0 * PI * f;
 
         let num = 1.0;
-        let denom_real = 1.0 - omega_sq * l * c;
-        let denom_imag = omega * r * c;
+        let denom_real = 1.0 - pulse * pulse * l * c;
+        let denom_imag = pulse * r * c;
 
         let mag = num / (denom_real.powi(2) + denom_imag.powi(2)).sqrt();
         let ph = -(denom_imag / denom_real).atan();
@@ -30,28 +29,48 @@ fn rlc_load_response(frequencies: &[f64], r: f64, l: f64, c: f64) -> (Vec<f64>, 
 }
 
 fn main() {
-    const L: f64 = 10e-3; // 10 mH
+    const L: f64 = 0.1e-3; // 10 mH
     const C: f64 = 1e-6; // 1 uF
 
-    let params = [
-        (10.0, "mag10", "phase10"),
-        (40.0, "mag40", "phase40"),
-        (100.0, "mag100", "phase100"),
+    let series = [
+        (1.0, "mag1", "phase1", "R = 1 Ω"),
+        (10.0, "mag2", "phase2", "R = 10 Ω"),
+        (100.0, "mag3", "phase3", "R = 100 Ω"),
     ];
 
     let mut source = data::NamedOwnedColumns::new();
-
-    let freq = common::logspace(100.0, 10000.0, 500);
-    for (r, mag_col, phase_col) in params {
-        let (mag, phase) = rlc_load_response(&freq, r, L, C);
-        source.add_column(mag_col, Box::new(mag));
-        source.add_column(phase_col, Box::new(phase));
-    }
-    source.add_column("freq", Box::new(freq));
 
     let filename = common::example_res("rlc-bode.eplt");
     let content = std::fs::read_to_string(&filename).unwrap();
 
     let figs = eplt::parse_diag(&content, Some(&filename)).unwrap();
-    common::save_figure(&figs[0], &source, "bode_rlc");
+    let mut fig = figs.into_iter().next().unwrap();
+
+    let freq = common::logspace(100.0, 1000000.0, 500);
+    for (r, mag_col, phase_col, name) in series {
+        let (mag, phase) = rlc_load_response(&freq, r, L, C);
+
+        source.add_column(mag_col, Box::new(mag));
+        source.add_column(phase_col, Box::new(phase));
+
+        let plots = fig.plots_mut().plots_mut();
+        plots[0].push_series(
+            ir::series::Line::new(
+                ir::DataCol::SrcRef("freq".to_string()),
+                ir::DataCol::SrcRef(mag_col.to_string()),
+            )
+            .with_name(name.to_string())
+            .into(),
+        );
+        plots[1].push_series(
+            ir::series::Line::new(
+                ir::DataCol::SrcRef("freq".to_string()),
+                ir::DataCol::SrcRef(phase_col.to_string()),
+            )
+            .with_name(name.to_string())
+            .into(),
+        );
+    }
+    source.add_column("freq", Box::new(freq));
+    common::save_figure(&fig, &source, "bode_rlc");
 }
